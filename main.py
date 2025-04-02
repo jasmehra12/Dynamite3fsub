fba0a6aae91c0e86feb3efc56423de029e3f2af0
jasmehra12 <jaskaran.1230j@gmail.com>
2025-04-02 16:25:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 from bot import Bot
 import asyncio
 import aiohttp
-
-URL = "https://rolling-nikkie-drxyhacker12-3c8942d4.koyeb.app/"
-async def ping():
-    async with aiohttp.ClientSession() as session:
-        while True:
-            try:
-                async with session.get(URL) as response:
-                    print(f"Pinged server, status: {response.status}")
-            except Exception as e:
-                print(f"{e}")
-            await asyncio.sleep(600)
-loop = asyncio.get_event_loop()
-loop.create_task(ping())
-Bot.run()
+Bot().run()
